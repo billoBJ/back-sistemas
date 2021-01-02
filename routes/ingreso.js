0516ac5887f0918dd752d7582de386a8b6a8901e
billoBJ <abillordo402@gmail.com>
2021-01-02 20:03:25 -0300

    ingreso Routes

---
+++ routes/ingreso.js
@@ -0,0 +1,13 @@
+import routerx from 'express-promise-router'
+import ingresoController from '../controllers/ingresoController'
+import auth from '../middlewares/auth'
+
+const router = routerx()
+
+router.post('/add',auth.verifyVendedor , ingresoController.add )
+router.get('/query',auth.verifyVendedor, ingresoController.query )
+router.get('/list',auth.verifyVendedor, ingresoController.list )
+router.put('/activate',auth.verifyVendedor, ingresoController.activate)
+router.put('/deactivate',auth.verifyVendedor, ingresoController.desactivate)
+
+export default router
